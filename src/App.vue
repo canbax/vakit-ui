83ab462c749af896cd64feb6858d31bf5475992e
canbax <yusufsaidcanbaz@gmail.com>
2024-05-04 18:56:31 +0300

    move ip2location info footer to main route page

--- src/App.vue
+++ src/App.vue
@@ -44,17 +44,5 @@ watch(currentPathMenuItem, () => {
     <v-main>
       <component :is="currentView" :key="currentLanguage" />
     </v-main>
-    <v-footer :app="true">
-      <div class="text-decoration-none font-x-small">
-        This site or product includes IP2Location LITE data available from
-        <a href="https://lite.ip2location.com">https://lite.ip2location.com</a>.
-      </div>
-    </v-footer>
   </v-app>
 </template>
-
-<style scoped>
-.font-x-small {
-  font-size: x-small;
-}
-</style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -83,6 +83,16 @@ function openGithub() {
       </v-card>
     </v-row>
   </v-container>
+  <v-footer :app="true">
+    <div class="text-decoration-none font-x-small">
+      This site or product includes IP2Location LITE data available from
+      <a href="https://lite.ip2location.com">https://lite.ip2location.com</a>.
+    </div>
+  </v-footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+.font-x-small {
+  font-size: x-small;
+}
+</style>
